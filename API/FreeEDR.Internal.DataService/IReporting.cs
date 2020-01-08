1696a8e5e3c4cf90b5d3c416520ea4eef8e6fef0
DESKTOP-7BTGJ2T\Horger <mh3294@drexel.edu>
2020-01-08 10:21:33 -0500

    Initial structure for internal API
    abstracted framework, 3 distinct services created

---
+++ API/FreeEDR.Internal.DataService/IReporting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FreeEDR.Internal.DataService
+{
+    [ServiceContract]
+    public interface IReporting
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+    }
+
+}
