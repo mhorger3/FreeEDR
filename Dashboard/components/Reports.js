5280d088c2fa62f122e0945b4c64d3249fb632c7
DESKTOP-7BTGJ2T\Horger <mh3294@drexel.edu>
2020-02-15 16:38:43 -0500

    Adding tabs

--- Dashboard/components/Reports.js
+++ Dashboard/components/Reports.js
@@ -2,19 +2,38 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import MUIDataTable from 'mui-datatables';
+import {AppBar, Tab, Tabs} from '@material-ui/core';
 export class Reports extends Component {
     constructor(props){
 		super(props);
 		this.state = {
-		
+			value: 0
 		};
+		this.handleChange = this.handleChange.bind(this);
 	};
+
+	handleChange(event, value){
+	    this.setState({
+	    	 value: value 
+		});
+	}
 
 	render(){
 		const { site } = this.props;
+		const { value } = this.state;
 		return (
 			<div id="descriptionContent">
 				<h3> FreeEDR Senior Design Reports Page </h3>
+				<AppBar position="static">
+				<Tabs value={value} onChange={this.handleChange} aria-label="simple tabs example">
+					<Tab label="Item One"/>
+					<Tab label="Item Two"/>
+					<Tab label="Item Three"/>
+				</Tabs>
+				</AppBar>
+				{value === 0 && <div id="content">a </div>}
+				{value === 1 && <div id="content">b  </div>}
+				{value === 2 && <div id="content">c  </div>}
 			</div>
 		)
 	}
